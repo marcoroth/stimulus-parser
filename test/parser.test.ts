3c331885e77c68f9b5c363f33283990e41c19efe
Marco Roth <marco.roth@intergga.ch>
2023-12-26 00:39:01 +0100

    Add test case for private getter

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -160,7 +160,24 @@ test("parse controller with public class fields", () => {
     }
   `
 
-  const controller = parser.parseController(code, "target_controller.js")
+  const controller = parser.parseController(code, "controller.js")
 
   expect(controller.parseError).toBeUndefined()
+})
+
+test("parse controller with private getter", () => {
+  const code = `
+    import { Controller } from "@hotwired/stimulus"
+
+    export default class extends Controller {
+      get #privateGetter () {
+        return true
+      }
+    }
+  `
+
+  const controller = parser.parseController(code, "controller.js")
+
+  expect(controller.parseError).toBeUndefined()
+  expect(controller.methods).toEqual([])
 })
