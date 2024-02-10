f8a8c05937783b358276a37df1a3e341df2e5462
Marco Roth <marco.roth@intergga.ch>
2024-02-10 02:46:24 +0100

    Add stub test file for parent controller inheritance

---
+++ test/parser/parent.inheritance.test.ts
@@ -0,0 +1,26 @@
+import { expect, test, describe } from "vitest"
+// import { setupParser } from "../helpers/setup"
+
+// const parser = setupParser()
+
+describe("inheritance", () => {
+  test.skip("inherits methods", () => {
+    expect(true).toBeTruthy()
+  })
+
+  test.skip("inherits targets", () => {
+    expect(true).toBeTruthy()
+  })
+
+  test.skip("inherits values", () => {
+    expect(true).toBeTruthy()
+  })
+
+  test.skip("inherits classes", () => {
+    expect(true).toBeTruthy()
+  })
+
+  test.skip("inherits outlets", () => {
+    expect(true).toBeTruthy()
+  })
+})
