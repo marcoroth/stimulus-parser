f9dd3bfca3fd8b980d78530b2a518790c447da8b
Marco Roth <marco.roth@intergga.ch>
2024-02-16 00:07:19 +0100

    Add fixtures for all kinds of Rails app setups

---
+++ test/fixtures/shakapacker/config/webpack/webpack.config.js
@@ -0,0 +1,7 @@
+// See the shakacode/shakapacker README and docs directory for advice on customizing your webpackConfig.
+
+const { generateWebpackConfig } = require("shakapacker")
+
+const webpackConfig = generateWebpackConfig()
+
+module.exports = webpackConfig
---
+++ test/fixtures/webpacker/app/javascript/controllers/index.js
@@ -0,0 +1,8 @@
+// Load all the controllers within this directory and all subdirectories.
+// Controller files must be named *_controller.js.
+
+import { application } from "./application"
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
