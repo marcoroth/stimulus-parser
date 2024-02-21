a3d8129e2fd3f074ae947720933ef2584602cc5c
Tony Messias <tonysm@hey.com>
2024-02-20 21:27:25 -0300

    Adds Laravel importmap eager and lazy loading examples

---
+++ test/fixtures/importmap-laravel-lazy/resources/js/controllers/hello_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static values = {
+        message: { type: String, default: 'Hello World' },
+    }
+
+    static targets = ['output']
+
+    connect() {
+        this.#updateElement(this.messageValue)
+    }
+
+    update({ params: { message } }) {
+        this.#updateElement(message)
+    }
+
+    #updateElement(message) {
+        this.outputTarget.textContent = message
+    }
+}
