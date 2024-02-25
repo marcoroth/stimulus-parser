8b937bad53f0821068616ec7f705b945d91a3bbf
Marco Roth <marco.roth@intergga.ch>
2024-02-25 17:43:15 +0100

    Short-circuit fixtures setup on production

--- scripts/setupFixtures.mjs
+++ scripts/setupFixtures.mjs
@@ -2,6 +2,8 @@ import path from "path"
 import { glob } from "glob"
 import { execSync } from "child_process"
 
+if (process.env.NODE_ENV === "production") process.exit(0)
+
 const fixtures = await glob("test/fixtures/**/package.json", { ignore: "**/**/node_modules/**" })
 
 fixtures.forEach(async fixturesPath => {
