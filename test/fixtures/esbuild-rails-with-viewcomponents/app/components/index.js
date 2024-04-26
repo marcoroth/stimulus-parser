8614abed544182372fdbcc235fe3eb0eda462a64
Marco Roth <marco.roth@intergga.ch>
2024-04-26 14:46:32 +0200

    Add failing test for ViewComponent setup

---
+++ test/fixtures/esbuild-rails-with-viewcomponents/app/components/index.js
@@ -0,0 +1,7 @@
+import { application } from "../javascript/controllers/application"
+
+import controllers from "./**/*_controller.js"
+
+controllers.forEach((controller) => {
+  application.register(controller.name, controller.module.default)
+})
---
+++ test/system/esbuild-rails-with-viewcomponents.test.ts
@@ -0,0 +1,37 @@
+import { describe, test, expect } from "vitest"
+import { setupProject } from "../helpers/setup"
+
+const project = setupProject("esbuild-rails-with-viewcomponents")
+
+describe("System", () => {
+  test("esbuild-rails-with-viewcomponents", async () => {
+    expect(project.controllersFile).toBeUndefined()
+    expect(project.applicationFile).toBeUndefined()
+    expect(project.registeredControllers.length).toEqual(0)
+
+    await project.initialize()
+
+    expect(project.applicationFile).toBeDefined()
+    expect(project.applicationFile.localApplicationConstant).toEqual("application")
+    expect(project.applicationFile.exportedApplicationConstant).toEqual("application")
+    expect(project.relativePath(project.applicationFile.path)).toEqual("app/javascript/controllers/application.js")
+
+    expect(project.controllersFile).toBeDefined()
+    expect(project.controllersFile.applicationImport).toBeDefined()
+    expect(project.controllersFile.localApplicationConstant).toEqual("application")
+
+    expect(project.relativePath(project.controllersFile.path)).toEqual("app/javascript/controllers/index.js")
+    // expect(project.relativePath(project.controllersFile.path)).toEqual("app/components/index.js")
+
+    expect(project.registeredControllers.length).toEqual(3)
+    expect(project.registeredControllers.map(controller => [controller.identifier, controller.loadMode])).toEqual([
+      ["hello", "esbuild-rails"],
+      ["message", "esbuild-rails"],
+      ["comment--component", "esbuild-rails"],
+    ])
+    expect(Array.from(project.controllerRoots)).toEqual([
+      "app/components",
+      "app/javascript/controllers",
+    ])
+  })
+})
