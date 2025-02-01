5ef2a86d50500cb817cbeb08587f99d4d4e60523
Marco Roth <marco.roth@intergga.ch>
2025-02-01 18:18:51 +0100

    Enable `erasableSyntaxOnly` option (#278)

--- src/parse_error.ts
+++ src/parse_error.ts
@@ -1,12 +1,22 @@
 import type { SourceLocation } from "acorn"
 
 export class ParseError {
+  public readonly severity: "LINT" | "FAIL";
+  public readonly message: string;
+  public readonly loc?: SourceLocation | null;
+  public readonly cause?: Error;
+
   constructor(
-    public readonly severity: "LINT" | "FAIL",
-    public readonly message: string,
-    public readonly loc?: SourceLocation | null,
-    public readonly cause?: Error,
-  ) {}
+    severity: "LINT" | "FAIL",
+    message: string,
+    loc?: SourceLocation | null,
+    cause?: Error,
+  ) {
+    this.severity = severity;
+    this.message = message;
+    this.loc = loc;
+    this.cause = cause;
+  }
 
   get inspect(): object {
     return {
