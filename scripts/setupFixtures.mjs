d9d4cc8b717133588733acb930e673f827ff4a56
Marco Roth <marco.roth@intergga.ch>
2026-05-23 17:50:26 +0200

    Bump to Node 26, Drop Node.js in CI

--- scripts/setupFixtures.mjs
+++ scripts/setupFixtures.mjs
@@ -4,10 +4,13 @@ import { execSync } from "child_process"
 
 const fixtures = await glob("test/fixtures/**/package.json", { ignore: "**/**/node_modules/**" })
 
-fixtures.forEach(async fixturesPath => {
+for (const fixturesPath of fixtures) {
   const fixtureFolder = path.dirname(fixturesPath)
 
   console.log(`Installing packages for fixture: ${fixtureFolder}`)
 
-  execSync(`cd ${fixtureFolder} && yarn install && cd -`)
-})
+  execSync("yarn install", {
+    cwd: fixtureFolder,
+    stdio: ["ignore", "ignore", "ignore"],
+  })
+}
